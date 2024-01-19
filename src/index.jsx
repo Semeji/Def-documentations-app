2c264b78f2daa59038e8ce358e05d41b8f69a385
Semeji <135881633+Semeji@users.noreply.github.com>
2024-01-19 11:59:19 +0000

    create pages and components, and make configuration

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     element: <Layout />,
     children: [
       {
-        path: "/authentification",
+        path: "/",
         element: <Authentification />,
       },
       {
