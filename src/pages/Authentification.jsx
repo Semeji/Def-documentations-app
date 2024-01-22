0a33d36dbb678c7ebcb2795fe62c47694838643a
Semeji <135881633+Semeji@users.noreply.github.com>
2024-01-22 10:40:19 +0000

    Adding content

--- src/pages/Authentification.jsx
+++ src/pages/Authentification.jsx
@@ -188,39 +188,138 @@ const Authentification = () => {
       <h2 id="Création de l’espace de travail">
         Création de l’espace de travail
       </h2>
-      <img src={capture} alt='Capture' width='50' height='100'>
-      </img>
+            <img src={capture} alt='Capture' width='50' height='100'/>
         </div>
           
           <div className='Content'>
             <h2 id="Choix de la catégorie de l’assujetti">
               Choix de la catégorie de l’assujetti
             </h2>
-          <img src={capture} alt='Capture' width='50' height='100'>
-          </img>
+            <img src={capture} alt='Capture' width='50' height='100'/>
             </div>
           
           <div className='Content'>
           <h2 id="Personne physique">
             Personne physique
           </h2>
+            <p>Apres le choix de la catégorie de l’assujetti, si cette dernière est une personne 
+            physique (commerçante ou non commerçante) ; il est important de préciser que la 
+            procédure est la même.</p>
+            <div className='Content'>
+              <h3 id="Choix de la catégorie de l’assujetti">
+                Etape 1 : enregistrement de coordonnées 
+              </h3>
+              <img src={capture} alt='Capture' width='50' height='100'/>
+              </div>
+
+      
+
+            <div className='Content'>
+              <h3 id="Choix de la catégorie de l’assujetti">
+                Etape 2: choix de la nationalité 
+              </h3>
+              <img src={capture} alt='Capture' width='50' height='100'/>
+              </div>
+
+            <div className='Content'>
+              <h3 id="Choix de la catégorie de l’assujetti">
+                Assujetti de droit congolais
+              </h3>
+              <img src={capture} alt='Capture' width='50' height='100'/>
+              </div>
+
+            <div className='Content'>
+            <h3 id="Choix de la catégorie de l’assujetti">
+              Etape 3 : Déclinaison de l'identité de la personne 
+            </h3>
+              <img src={capture} alt='Capture' width='50' height='100'/>
+              
+            </div>
+            <p>N.B : Après avoir soumis le formulaire, le système génère de manière automatique le 
+            nom d’utilisateur et le mot de passe par défaut après l’enregistrement via l’adresse mail 
+            renseignée à l’étape 1.
+</p>
+            
             </div>
 
-          <div className='Content'>
-            <h2 id="Personne physique">
-              Personne morale
-            </h2>
+            <div className='Content'>
+              <h2 id="Personne physique">
+                Personne morale
+              </h2>
+                <p>Apres le choix de la catégorie de l’assujetti, si cette dernière est une personne 
+                morale (commerçante ou non commerçante) ; il est important de préciser que la 
+                procédure est la même à la première, deuxième et quatrième étape.
+</p>
+                <div className='Content'>
+                  <h3 id="Choix de la catégorie de l’assujetti">
+                    Etape 1 : enregistrement de coordonnées 
+                  </h3>
+                <img src={capture} alt='Capture' width='50' height='100'/>
+                  </div>
+
+
+
+                <div className='Content'>
+                  <h3 id="Choix de la catégorie de l’assujetti">
+                    Etape 2: choix de la nationalité 
+                  </h3>
+                <img src={capture} alt='Capture' width='50' height='100'/>
+                  </div>
+
+                <div className='Content'>
+                  <h3 id="Choix de la catégorie de l’assujetti">
+                    Assujetti de droit congolais
+                  </h3>
+                <img src={capture} alt='Capture' width='50' height='100'/>
+                  </div>
+
+                <div className='Content'>
+                <h3 id="Choix de la catégorie de l’assujetti">
+                  Etape 3 : Déclinaison de l'identité de la personne 
+                </h3>
+                <img src={capture} alt='Capture' width='50' height='100'/>
+                  <img src={capture} alt='Capture' width='50' height='100'/>
+
+                  
+                </div>
+              <div className='Content'>
+                <h3 id="Choix de la catégorie de l’assujetti">
+                  Etape 4 : déclinaison de l’identité 
+
+                </h3>
+                <img src={capture} alt='Capture' width='50' height='100'/>
+                  <img src={capture} alt='Capture' width='50' height='100'/>
+
+                </div>
+              <p>N.B : Après avoir soumis le formulaire, le système génère de manière automatique le 
+                nom d’utilisateur et le mot de passe par défaut après l’enregistrement via l’adresse mail 
+                renseignée à l’étape 1.
+
+              </p>
               </div>
           
           </div>
         </section>
 
+
+
+      <section className='Content'>
+
+        <h2 id="Connexion au système">
+          Connexion au système
+        </h2>
+        <p>Après l’enregistrement dans le DEF, le nom d’utilisateur et le mot de passe par 
+        défaut reçus via le mail doivent être renseignés respectivement dans les champs cidessous pour se connecter 
+        </p>
+        
+        <div className='Content'>
+      <img src={capture} alt='Capture' width='50' height='100'/>
+     
+        </div>
+
+        </section>
+    
       
-    
-    
-      <h2 id="Connexion au système">
-        Connexion au système
-      </h2>
       
       
     </div>
