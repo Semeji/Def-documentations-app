defee34f4cd1cbe9585f104e1dfb53ebaf7bd3c3
Semeji <135881633+Semeji@users.noreply.github.com>
2024-02-05 12:41:45 +0000

    Screen update position

--- src/pages/Screens.jsx
+++ src/pages/Screens.jsx
@@ -1,18 +1,46 @@
-import React from 'react';
-
+import React from "react";
+import CreateCount from "../images/CreateCount.png";
+import CatAssujetti from "../images/CatAssujetti.png";
+import StorageData from "../images/StorageData.png";
+import SelectNationnality from "../images/SelectNationnality.png";
+import AssujettiCongolais from "../images/AssujettiCongolais.png";
+import AssujettiEtranger from "../images/AssujettiEtranger.png";
+import IdentitéPersonnelle from "../images/IdentitéPersonnelle.png";
+import PMC from "../images/PMC.png";
+import PMNC from "../images/PMNC.png";
+import DenominationEts from "../images/DenominationEts.png";
+import StatutNotarie from "../images/StatutNotarie.png";
+import Connexion from "../images/Connexion.png";
 
 const Screens = () => {
+  const handleScroll = () => {
+    document.addEventListener("scroll", function () {
+      const sectionParagraph = document.querySelector(".Procedures");
+      const sectionImage = document.querySelector("#préambule");
+      console.log("je t'aime", sectionImage);
+      if (window.scrollY > sectionParagraph.offsetTop) {
+        sectionImage.style.visibility = "visible";
+      } else {
+        sectionImage.style.visibility = "hidden";
+      }
+    });
+    s;
+  };
+
   return (
-      <h1>
-        Screens
-      </h1>
-
+    <div className="position right-0">
+      <h1>Screens</h1>
+      <section className="sectionImage">
+        <img
+          src={CreateCount}
+          alt="Capture"
+          className="w-[30%] h-[30%]  "
+          onScroll={handleScroll}
+          id="Image-CreateCount"
+        />
+      </section>
+    </div>
   );
-}
+};
 
-export default Screens;;
-
-
-
-
-
+export default Screens;
